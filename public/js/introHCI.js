4e66e781bb27c8df69b6f89f4848a34777ae12f2
Debbie Vo <debbienvo@yahoo.com>
2019-02-20 12:42:41 -0800

    Lab 7 Analytics and AB testing

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,13 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(clickLikeBtn);
+}
+
+function clickLikeBtn(){
+	//gtag('create', 'UA-134888574-1', 'auto');
+	//gtag('send', 'event', 'like', 'click');
+	
+	//This one works
+	gtag('event','click', {'event_category': 'like', 'event_label': 'click'});
 }
